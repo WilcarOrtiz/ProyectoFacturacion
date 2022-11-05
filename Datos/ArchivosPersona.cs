6378bb2b827255214ed27366ebd30ad8b616502d
MSlegro <legromanuel29@gmail.com>
2022-11-05 16:52:57 -0500

    Capa Datos acabada

---
+++ Datos/ArchivosPersona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Datos
+{
+    public class ArchivosPersona
+    {
+        protected String ruta;
+
+        public ArchivosPersona()
+        {
+            ruta = "Archivo.txt";
+        }
+
+        public ArchivosPersona(String FileName)
+        {
+            ruta = FileName;
+        }
+
+        public String GuardarEmpleado(Empleado Persona)
+        {
+            StreamWriter sw = new StreamWriter(ruta, true);
+            sw.WriteLine(Persona.ToString());
+            sw.Close();
+            return "Correcto";
+        }
+
+        public String GuardarClientes(Cliente Persona)
+        {
+            StreamWriter sw = new StreamWriter(ruta, true);
+            sw.WriteLine(Persona.ToString());
+            sw.Close();
+            return "Correcto";
+        }
+
+        public String GuardarUsuario(Usuario Persona)
+        {
+            StreamWriter sw = new StreamWriter(ruta, true);
+            sw.WriteLine(Persona.ToString());
+            sw.Close();
+            return "Correcto";
+        }
+    }
+}
---
+++ Datos/RepositorioProducto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Datos
+{
+    public class RepositorioProducto : ArchivosProducto
+    {
+        public RepositorioProducto() : base()
+        {
+            ruta = "ListaProductos.TXT";
+        }
+
+        public RepositorioProducto(String FileName) : base(FileName) { }
+
+        public List<Producto> GetAll()
+        {
+            StreamReader sr = new StreamReader(ruta);
+            List<Producto> products = new List<Producto>();
+            while (!sr.EndOfStream)
+            {
+                products.Add(Mappear(sr.ReadLine()));
+            }
+            sr.Close();
+            return products;
+        }
+
+        Producto Mappear(String linea)
+        {
+            var Articulo = new Producto();
+            Articulo.ID = linea.Split(';')[0];
+            Articulo.Codigo = linea.Split(';')[1];
+            Articulo.NombreProducto = linea.Split(';')[2];
+            Articulo.Descripcion = linea.Split(';')[3];
+            Articulo.Cantidad = int.Parse(linea.Split(';')[4]);
+            Articulo.Unidad = linea.Split(';')[5];
+            Articulo.PrecioC = float.Parse(linea.Split(';')[6]);
+            Articulo.PrecioV = float.Parse(linea.Split(';')[7]);
+            return Articulo;
+        }
+
+        public string Actualizar(List<Producto> products, bool Modo)
+        {
+
+            var sw = new StreamWriter(ruta, Modo);
+            foreach (var item in products)
+            {
+                sw.WriteLine(item.ToString());
+            }
+            sw.Close();
+            return "Correcto";
+
+        }
+    }
+}
