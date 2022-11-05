9c3f5f21598a2fbaf604f82958de01b29c56d68f
MSlegro <legromanuel29@gmail.com>
2022-11-05 17:15:31 -0500

    Logica acabada

---
+++ Logica/FuncionesEmpleado.cs
@@ -0,0 +1,51 @@
+﻿using Datos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class FuncionesEmpleado : IServiciosPersona<Empleado>
+    {
+        List<Empleado> empleados;
+        RepositorioEmpleado repositorioEmpleado = new RepositorioEmpleado();
+
+        public FuncionesEmpleado()
+        {
+            empleados = new List<Empleado>();
+            empleados = repositorioEmpleado.GetAll();
+        }
+        public string Agregar(Empleado persona)
+        {
+            String Mensaje;
+            try
+            {
+                empleados.Add(persona);
+                Mensaje = repositorioEmpleado.GuardarEmpleado(persona);
+            }
+            catch (Exception EX)
+            {
+                Mensaje = " " + EX;
+            }
+            return Mensaje;
+        }
+
+        public string Editar(Empleado persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Eliminar(Empleado persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleado> GetAllUsuarios()
+        {
+            return repositorioEmpleado.GetAll();
+        }
+    }
+}
---
+++ Logica/IServiciosPersona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public interface IServiciosPersona<T>
+    {
+        String Agregar(T persona);
+
+        String Editar(T persona);
+
+        List<T> GetAllUsuarios();
+
+        String Eliminar(T persona);
+    }
+}
