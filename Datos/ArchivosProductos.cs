1b32061a18b37721fcfd809cd35113344e937bc5
MSlegro <legromanuel29@gmail.com>
2022-11-05 16:18:01 -0500

    Agregar Entidades

---
+++ Datos/ArchivosProductos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ArchivosProductos
+    {
+        protected String ruta;
+
+        public ArchivosProductos()
+        {
+            ruta = "Producto.txt";
+        }
+        public ArchivosProductos(string FileName)
+        {
+            ruta = FileName;
+        }
+
+        public String GuardarProducto(Entitades.Producto Articulo)
+        {
+            StreamWriter sw = new StreamWriter(ruta, true);
+            sw.WriteLine(Articulo.ToString());
+            sw.Close();
+            return "Correcto";
+        }
+
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario : Empleado
+    {
+        public String NombreUser { get; set; }
+        public String Contraseña { get; set; }
+
+
+        public override string ToString()
+        {
+            return NombreUser + ";" + Contraseña + ";" + Cedula;
+        }
+    }
+}
