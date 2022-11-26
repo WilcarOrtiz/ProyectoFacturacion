a346b18f37181a2da14c30060e6dd621fa779d66
MSlegro <legromanuel29@gmail.com>
2022-11-25 22:19:32 -0500

    Clientes

---
+++ Presentacion_GUI/FrmClientes.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Presentacion_GUI
+{
+    partial class FrmClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmClientes));
+            this.GrillaClientes = new System.Windows.Forms.DataGridView();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnImprimir = new FontAwesome.Sharp.IconButton();
+            this.cmbTipoBusqueda = new System.Windows.Forms.ComboBox();
+            this.IconoBuscar = new FontAwesome.Sharp.IconPictureBox();
+            this.textBusqueda = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Editar = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GrillaClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconoBuscar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GrillaClientes
+            // 
+            this.GrillaClientes.AllowUserToOrderColumns = true;
+            this.GrillaClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.GrillaClientes.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.GrillaClientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.GrillaClientes.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.GrillaClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.GrillaClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GrillaClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Editar});
+            this.GrillaClientes.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.GrillaClientes.Location = new System.Drawing.Point(119, 146);
+            this.GrillaClientes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GrillaClientes.Name = "GrillaClientes";
+            this.GrillaClientes.ReadOnly = true;
+            this.GrillaClientes.RowHeadersWidth = 62;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            this.GrillaClientes.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.GrillaClientes.Size = new System.Drawing.Size(1160, 450);
+            this.GrillaClientes.TabIndex = 73;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Azure;
+            this.label10.Location = new System.Drawing.Point(194, 49);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 20);
+            this.label10.TabIndex = 70;
+            this.label10.Text = "Buscar por";
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.FlatAppearance.BorderSize = 0;
+            this.btnImprimir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImprimir.ForeColor = System.Drawing.Color.Azure;
+            this.btnImprimir.IconChar = FontAwesome.Sharp.IconChar.FilePdf;
+            this.btnImprimir.IconColor = System.Drawing.Color.Azure;
+            this.btnImprimir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnImprimir.IconSize = 32;
+            this.btnImprimir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImprimir.Location = new System.Drawing.Point(1038, 33);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnImprimir.Size = new System.Drawing.Size(241, 53);
+            this.btnImprimir.TabIndex = 75;
+            this.btnImprimir.Text = "Descargar PDF";
+            this.btnImprimir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImprimir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // cmbTipoBusqueda
+            // 
+            this.cmbTipoBusqueda.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoBusqueda.FormattingEnabled = true;
+            this.cmbTipoBusqueda.Items.AddRange(new object[] {
+            "Codigo",
+            "Nombre",
+            "Estado"});
+            this.cmbTipoBusqueda.Location = new System.Drawing.Point(378, 46);
+            this.cmbTipoBusqueda.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbTipoBusqueda.Name = "cmbTipoBusqueda";
+            this.cmbTipoBusqueda.Size = new System.Drawing.Size(141, 28);
+            this.cmbTipoBusqueda.TabIndex = 71;
+            // 
+            // IconoBuscar
+            // 
+            this.IconoBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.IconoBuscar.ForeColor = System.Drawing.Color.Azure;
+            this.IconoBuscar.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlassArrowRight;
+            this.IconoBuscar.IconColor = System.Drawing.Color.Azure;
+            this.IconoBuscar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.IconoBuscar.Location = new System.Drawing.Point(322, 44);
+            this.IconoBuscar.Name = "IconoBuscar";
+            this.IconoBuscar.Size = new System.Drawing.Size(32, 32);
+            this.IconoBuscar.TabIndex = 74;
+            this.IconoBuscar.TabStop = false;
+            // 
+            // textBusqueda
+            // 
+            this.textBusqueda.Location = new System.Drawing.Point(543, 46);
+            this.textBusqueda.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBusqueda.Name = "textBusqueda";
+            this.textBusqueda.Size = new System.Drawing.Size(341, 26);
+            this.textBusqueda.TabIndex = 72;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 379);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1419, 291);
+            this.panel1.TabIndex = 76;
+            // 
+            // Editar
+            // 
+            this.Editar.HeaderText = "Editar";
+            this.Editar.MinimumWidth = 8;
+            this.Editar.Name = "Editar";
+            this.Editar.ReadOnly = true;
+            this.Editar.Width = 69;
+            // 
+            // FrmClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(1419, 670);
+            this.Controls.Add(this.GrillaClientes);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.cmbTipoBusqueda);
+            this.Controls.Add(this.IconoBuscar);
+            this.Controls.Add(this.textBusqueda);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmClientes";
+            this.Text = "FrmClientes";
+            this.Load += new System.EventHandler(this.FrmClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GrillaClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconoBuscar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GrillaClientes;
+        private System.Windows.Forms.Label label10;
+        private FontAwesome.Sharp.IconButton btnImprimir;
+        private System.Windows.Forms.ComboBox cmbTipoBusqueda;
+        private FontAwesome.Sharp.IconPictureBox IconoBuscar;
+        private System.Windows.Forms.TextBox textBusqueda;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewButtonColumn Editar;
+    }
+}
---
+++ Presentacion_GUI/FrmClientes.cs
@@ -0,0 +1,45 @@
+﻿using Entidades;
+using Logica;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion_GUI
+{
+    public partial class FrmClientes : Form
+    {
+        DataTable Tabla;
+        NuevasFuncionesCliente funcionesCliente = new NuevasFuncionesCliente(); 
+        public FrmClientes()
+        {
+            InitializeComponent();
+        } 
+
+        public void CargarGrilla()
+        {
+            Tabla = new DataTable();
+            Tabla.Columns.Add("Cedula");
+            Tabla.Columns.Add("Primer Nombre");
+            Tabla.Columns.Add("Segundo Nombre");
+            Tabla.Columns.Add("Correo");
+            Tabla.Columns.Add("Telefono");
+            Tabla.Columns.Add("Estado");
+
+            foreach (NCliente item in funcionesCliente.Listar())
+            {
+                Tabla.Rows.Add(item.Cedula,item.Nombre,item.Apellido,item.Correo,item.Telefono,item.PEstado.Descripcion);   
+            }
+        }
+
+        private void FrmClientes_Load(object sender, EventArgs e)
+        {
+            CargarGrilla();
+        }
+    }
+}
