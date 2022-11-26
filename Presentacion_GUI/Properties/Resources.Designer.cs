a847f38f12eb153ac2bf7ffac7c21bfa99e5f24c
WILCAR <ortizcolpaswilcardaniel@gmail.com>
2022-11-26 10:08:49 -0500

    Nuevos cambios

--- Presentacion_GUI/Properties/Resources.Designer.cs
+++ Presentacion_GUI/Properties/Resources.Designer.cs
@@ -82,6 +82,37 @@ namespace Presentacion_GUI.Properties {
         
         /// <summary>
         ///   Busca una cadena traducida similar a &lt;!DOCTYPE html&gt;
+        ///&lt;html xmlns=&quot;http://www.w3.org/1999/xhtml&quot;&gt;
+        ///&lt;head&gt;
+        ///    &lt;title&gt;Title of the document&lt;/title&gt;
+        ///    &lt;style&gt;
+        ///        table.border {
+        ///            border-collapse: collapse;
+        ///        }
+        ///
+        ///            table.border th {
+        ///                padding: 5px;
+        ///                border: 1px solid black;
+        ///            }
+        ///
+        ///            table.border td {
+        ///                padding: 5px;
+        ///                border: 1px solid black;
+        ///            }
+        ///    &lt;/style&gt;
+        ///&lt;/head&gt;
+        ///&lt;body&gt;
+        ///    &lt;table style=&quot;width:100%&quot;&gt;
+        ///        [resto de la cadena truncado]&quot;;.
+        /// </summary>
+        internal static string Plantilla {
+            get {
+                return ResourceManager.GetString("Plantilla", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a &lt;!DOCTYPE html&gt;
         ///&lt;html&gt;
         ///&lt;head&gt;
         ///    &lt;title&gt;Web Page Design&lt;/title&gt;
@@ -106,9 +137,9 @@ namespace Presentacion_GUI.Properties {
         ///&lt;body&gt;
         ///    &lt;tab [resto de la cadena truncado]&quot;;.
         /// </summary>
-        internal static string Plantilla {
+        internal static string PlantillaListadoClientes {
             get {
-                return ResourceManager.GetString("Plantilla", resourceCulture);
+                return ResourceManager.GetString("PlantillaListadoClientes", resourceCulture);
             }
         }
         
